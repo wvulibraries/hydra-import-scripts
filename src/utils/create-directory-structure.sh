e78021c1031cc31df22ac018517cc97a072969c0
Michael Bond <mbond@the-forgotten.org>
2016-11-21 07:39:46 -0500

    Remove command line arg, and use the HYDRA_PROJECT_NAME env variable
    if the env varible doesn't exist, exit with instructions.

--- src/utils/create-directory-structure.sh
+++ src/utils/create-directory-structure.sh
@@ -1,8 +1,13 @@
 #!/bin//bash
 
-mkdir -p /mnt/nfs-exports/mfcs-exports/$1/control/
-mkdir -p /mnt/nfs-exports/mfcs-exports/$1/control/hydra/error
-mkdir -p /mnt/nfs-exports/mfcs-exports/$1/control/hydra/finished
-mkdir -p /mnt/nfs-exports/mfcs-exports/$1/control/hydra/in-progress
-mkdir -p /mnt/nfs-exports/mfcs-exports/$1/control/hydra/staged
-mkdir -p /mnt/nfs-exports/mfcs-exports/$1/export
+if [[ -z $HYDRA_PROJECT_NAME ]]; then
+  echo "ENV variable HYDRA_PROJECT_NAME must be defined. Use add_project_env."
+  exit 1
+fi
+
+mkdir -p /mnt/nfs-exports/mfcs-exports/"$HYDRA_PROJECT_NAME"/control/
+mkdir -p /mnt/nfs-exports/mfcs-exports/"$HYDRA_PROJECT_NAME"/control/hydra/error
+mkdir -p /mnt/nfs-exports/mfcs-exports/"$HYDRA_PROJECT_NAME"/control/hydra/finished
+mkdir -p /mnt/nfs-exports/mfcs-exports/"$HYDRA_PROJECT_NAME"/control/hydra/in-progress
+mkdir -p /mnt/nfs-exports/mfcs-exports/"$HYDRA_PROJECT_NAME"/control/hydra/staged
+mkdir -p /mnt/nfs-exports/mfcs-exports/"$HYDRA_PROJECT_NAME"/export
