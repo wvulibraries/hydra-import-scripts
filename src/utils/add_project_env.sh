30f9eb5fa2b6c24fe627dadaa66cb9c1ffbabc6b
Michael Bond <mbond@the-forgotten.org>
2016-11-21 07:39:12 -0500

    Error checking and source the enviornment
    Add basic error checking and usage, with example.
    Source the enviornment after running, so the shell doesn't need reloaded before running create-directory-structure.sh

--- src/utils/add_project_env.sh
+++ src/utils/add_project_env.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
+if [[ $# -ne 1 ]]; then
+    echo "Usage add_project_env.sh HYDRA_PROJECT_NAME"
+    echo "HYDRA_PROJECT_NAME is the name of the project, where the following directory exists"
+    echo "/home/HYDRA_PROJECT_NAME.lib.wvu.edu/HYDRA_PROJECT_NAME/"
+    echo "it is also the same value that is in the control_file.yaml, that is created with MFCS"
+    exit 1
+fi
+
 echo "HYDRA_PROJECT_NAME=$1" | tee -a /etc/environment
+source /etc/environment
