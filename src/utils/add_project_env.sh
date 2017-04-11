bb319ef358b3e082d94724dd61c0d91687851751
ddavisgraphics <dave@ddavisgraphics.com>
2017-04-11 08:45:26 -0400

    Changing directory strcuture to amtch new one for env vars

--- src/utils/add_project_env.sh
+++ src/utils/add_project_env.sh
@@ -3,7 +3,7 @@
 if [[ $# -ne 1 ]]; then
     echo "Usage add_project_env.sh HYDRA_PROJECT_NAME"
     echo "HYDRA_PROJECT_NAME is the name of the project, where the following directory exists"
-    echo "/home/HYDRA_PROJECT_NAME.lib.wvu.edu/HYDRA_PROJECT_NAME/"
+    echo "/home/HYDRA_PROJECT_NAME.lib.wvu.edu/hydra/"
     echo "it is also the same value that is in the control_file.yaml, that is created with MFCS"
     exit 1
 fi
