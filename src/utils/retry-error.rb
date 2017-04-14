6d99d34868d968b28262b5d35e8dda225543fcad
Michael Bond <mbond@the-forgotten.org>
2017-04-14 16:18:02 -0400

    Updated from testing
    1. Fixed `argv` vs `argv[0]`
    1. Fixed directory that file is loaded from
    1. Additional changes to assist in debuggging. 
    
    The following was tested:
    1. Tested all error states of the file. 
    1. Tested all success states of the file
    1. verified that thhe file was moved correctly on success and remained on failure.

--- src/utils/retry-error.rb
+++ src/utils/retry-error.rb
@@ -23,36 +23,30 @@ require 'fileutils'
 #  and if it passes the file is moved out of the error folder
 #  to the completed folder, if fails it is not moved.
 
-unless ENV.include? 'HYDRA_PROJECT_NAME'
-  abort "Missing ENV variable, 'HYDRA_PROJECT_NAME'"
-end
-
+env_var = "HYDRA_PROJECT_NAME";
 usage = 'Usage: ruby ./retry-error.rb {filename}'
+
+abort "ENV variable #{env_var} not set" unless ENV.include? env_var
 abort "Error: Expected filename.\n #{usage}" if ARGV.empty?
 abort "Error: Too many arguments.\n #{usage}" if ARGV.length > 1
 
-filename = ARGV
-error_dir = "/mnt/nfs-exports/mfcs-exports/#{ENV['HYDRA_PROJECT_NAME']}/control/hydra/error"
-success_dir = "/mnt/nfs-exports/mfcs-exports/#{ENV['HYDRA_PROJECT_NAME']}/control/hydra/finished"
+filename = ARGV[0]
+error_dir = "/mnt/nfs-exports/mfcs-exports/#{ENV[env_var]}/control/hydra/error"
+success_dir = "/mnt/nfs-exports/mfcs-exports/#{ENV[env_var]}/control/hydra/finished"
 
-unless File.exist?("#{error_dir}/#{filename}")
-  abort '#{filename} is missing make sure that it is correct and present in the error folder'
-end
+abort "#{filename} does not exist" unless File.exist?("#{error_dir}/#{filename}")
 
 config = YAML.load_file("#{error_dir}/#{filename}")
 
-if config['project_name'] != ENV['HYDRA_PROJECT_NAME']
-  abort 'Project name in control file does not match ENV HYDRA_PROJECT_NAME'
-end
+abort "Project name in control file does not match ENV '#{env_var}'" if config['project_name'] != ENV[env_var]
 
-Dir.chdir("/home/#{ENV['HYDRA_PROJECT_NAME']}.lib.wvu.edu/hydra/") do |dir_name|
-  export_locations = "/mnt/nfs-exports/mfcs-exports/#{config['project_name']}/export/#{config['time_stamp']}"
-  result = `/usr/local/bin/rails runner import/import.rb #{export_locations}`
+Dir.chdir("/home/#{ENV[env_var]}.lib.wvu.edu/hydra/") do |dir_name|
+  result = `/usr/local/bin/rails runner import/import.rb #{error_dir}/#{filename}`
 
   # looks at last bash command to see false status
   if $?.success?
     FileUtils.mv("#{error_dir}/#{config['time_stamp']}.yaml","#{success_dir}/#{config['time_stamp']}.yaml")
   else
-    abort "Error processing. Moved to error control directory. #{result}"
+    abort "Error processing. #{result}"
   end
 end
