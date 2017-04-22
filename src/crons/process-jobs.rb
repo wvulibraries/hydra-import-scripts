d30202f6d97ecc2f5cd764d7b75910d724a8ba2f
Michael Bond <mbond@the-forgotten.org>
2017-04-22 09:55:28 -0400

    testing

--- src/crons/process-jobs.rb
+++ src/crons/process-jobs.rb
@@ -45,10 +45,10 @@ Dir.chdir("/home/#{ENV['HYDRA_PROJECT_NAME']}.lib.wvu.edu/hydra/") do
   result = `/usr/local/bin/rails runner import/import.rb #{export_locations}`
   if (!$?.success?) then
     FileUtils.mv("#{in_process_dir}/control_file.yaml","#{error_dir}/#{config['time_stamp']}.yaml")
-    send_notifications(config['contact_emails'], "Import of #{config['project_name']} failed. #{$?.success?}")
+    send_notifications(config['contact_emails'], "Import of #{config['project_name']} failed. '#{Process.pid}' '#{$?.success?}'")
     abort "Error processing. Moved to error control directory. {#{result}}"
   else
-    send_notifications(config['contact_emails'], "Import of #{config['project_name']} succeeded. #{$?.success?}")
+    send_notifications(config['contact_emails'], "Import of #{config['project_name']} succeeded. '#{Process.pid}' '#{$?.success?}'")
     FileUtils.mv("#{in_process_dir}/control_file.yaml","#{success_dir}/#{config['time_stamp']}.yaml")
   end
 end
